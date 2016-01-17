a76f312227513b967265c7bf6a622c6662a7f767
Luca Zanrosso <lucazanrosso94@gmail.com>
2016-01-17 20:50:12 +0100

    Start restyling DayFragment

--- app/src/main/java/com/lucazanrosso/anguriara/DayAdapter.java
+++ app/src/main/java/com/lucazanrosso/anguriara/DayAdapter.java
@@ -10,12 +10,10 @@ import android.widget.TextView;
 
 public class DayAdapter extends RecyclerView.Adapter<DayAdapter.ViewHolder> {
     private static final int TYPE_ITEM = 1;
-    private static final int TYPE_DIVIDER = 2;
 
     private String[] titles;
     private String[] menuString;
     private int[] menuIcons;
-    private int specialType = 0;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         int holderId;
@@ -25,16 +23,11 @@ public class DayAdapter extends RecyclerView.Adapter<DayAdapter.ViewHolder> {
 
         public ViewHolder(View v, int viewType) {
             super(v);
-            if (viewType == TYPE_ITEM) {
-                mTitleTextView = (TextView) v.findViewById(R.id.day_title);
-                mTextView = (TextView) v.findViewById(R.id.day_text);
-                mImageView = (ImageView) v.findViewById(R.id.day_image);
-                mImageView.setColorFilter(Color.parseColor("#757575"));
-                holderId = 1;
-            }
-            if (viewType == TYPE_DIVIDER) {
-                holderId = 2;
-            }
+            mTitleTextView = (TextView) v.findViewById(R.id.day_title);
+            mTextView = (TextView) v.findViewById(R.id.day_text);
+            mImageView = (ImageView) v.findViewById(R.id.day_image);
+            mImageView.setColorFilter(Color.parseColor("#757575"));
+            holderId = 1;
         }
     }
 
@@ -46,43 +39,26 @@ public class DayAdapter extends RecyclerView.Adapter<DayAdapter.ViewHolder> {
 
     @Override
     public DayAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == TYPE_ITEM) {
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.day_row, parent, false);
-            ViewHolder vh = new ViewHolder(v, viewType);
-            return vh;
-        }
-        if (viewType == TYPE_DIVIDER) {
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.day_divider, parent, false);
-            ViewHolder vh = new ViewHolder(v, viewType);
-            return vh;
-        }
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.day_row, parent, false);
+        ViewHolder vh = new ViewHolder(v, viewType);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        if (holder.holderId == 1) {
-            holder.mTitleTextView.setText(titles[position - specialType]);
-            holder.mTextView.setText(menuString[position - specialType]);
-            holder.mImageView.setImageResource(menuIcons[position - specialType]);
-        }
-        if (holder.holderId == 2) {
-            specialType++;
-        }
+        holder.mTitleTextView.setText(titles[position]);
+        holder.mTextView.setText(menuString[position]);
+        holder.mImageView.setImageResource(menuIcons[position]);
     }
 
     @Override
     public int getItemCount() {
-        return menuString.length * 2 - 1;
+        return menuString.length;
     }
 
     @Override
     public int getItemViewType(int position) {
-        if ((position % 2) == 1) {
-            return TYPE_DIVIDER;
-        }
         return TYPE_ITEM;
     }
 }
