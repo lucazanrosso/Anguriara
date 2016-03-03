e0134ef532f4789f16f4b2535c5f6a58c42e8a2d
Luca Zanrosso <lucazanrosso94@gmail.com>
2016-03-03 22:30:33 +0100

    Fix the bug that happens when the day is only open

--- app/src/main/java/com/lucazanrosso/anguriara/DayFragment.java
+++ app/src/main/java/com/lucazanrosso/anguriara/DayFragment.java
@@ -12,10 +12,6 @@ import java.util.LinkedHashMap;
 
 public class DayFragment extends Fragment {
 
-    private int[] dayIcons = {R.drawable.ic_music_note_black_24dp,
-            R.drawable.ic_local_pizza_black_24dp,
-            R.drawable.ic_access_time_black_24dp};
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_day, container, false);
@@ -26,15 +22,19 @@ public class DayFragment extends Fragment {
 
         TextView eventTitle = (TextView) view.findViewById(R.id.event_title);
         TextView eventText = (TextView) view.findViewById(R.id.event_text);
-        if (day.get("event").isEmpty())
+        if (!day.get("event").isEmpty()) {
+            eventTitle.setText(getResources().getString(R.string.event));
+            eventText.setText(day.get("event") + " " + day.get("event_details"));
+        } else {
             eventTitle.setText(getResources().getString(R.string.open));
-        eventText.setText(day.get("event") + " " + day.get("event_details"));
+            ((ViewGroup) eventText.getParent()).removeView(eventText);
+        }
 
         TextView foodText = (TextView) view.findViewById(R.id.food_text);
-        if (day.get("food").isEmpty())
-            foodText.setText(getResources().getString(R.string.standard_foods));
-        else
+        if (!day.get("food").isEmpty())
             foodText.setText(day.get("food") + " " + getResources().getString(R.string.day_food) + "\n" + getResources().getString(R.string.standard_foods));
+        else
+            foodText.setText(getResources().getString(R.string.standard_foods));
 
         TextView openingTimeText = (TextView) view.findViewById(R.id.opening_time_text);
         if (!day.get("openingTime").isEmpty())
