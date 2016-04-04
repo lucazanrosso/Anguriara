8e1fd7355ae6582d96705f8c395e547ed7aa9536
Luca Zanrosso <lucazanrosso94@gmail.com>
2016-04-04 21:48:18 +0200

    Add sharing FAB in DayFragment

--- app/src/main/java/com/lucazanrosso/anguriara/DayScreenSlidePagerFragment.java
+++ app/src/main/java/com/lucazanrosso/anguriara/DayScreenSlidePagerFragment.java
@@ -1,6 +1,9 @@
 package com.lucazanrosso.anguriara;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -15,6 +18,15 @@ import java.util.GregorianCalendar;
 
 public class DayScreenSlidePagerFragment extends Fragment {
 
+    String[] daysOfWeek;
+    String[] months;
+    String thisDayOfWeek;
+    int thisDayOfMonth;
+    String thisMonth;
+    String title;
+
+    FloatingActionButton fab;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -23,13 +35,49 @@ public class DayScreenSlidePagerFragment extends Fragment {
         Bundle args = this.getArguments();
         GregorianCalendar date = (GregorianCalendar) args.getSerializable("date");
 
-        String[] daysOfWeek = getResources().getStringArray(R.array.days_of_week);
-        String[] months = getResources().getStringArray(R.array.months);
+        this.daysOfWeek = getResources().getStringArray(R.array.days_of_week);
+        this.months = getResources().getStringArray(R.array.months);
 
-        String thisDayOfWeek = daysOfWeek[date.get(Calendar.DAY_OF_WEEK) - 1];
-        int thisDayOfMonth = date.get(Calendar.DAY_OF_MONTH);
-        String thisMonth = months[date.get(Calendar.MONTH)];
-        MainActivity.toolbar.setTitle(thisDayOfWeek + " " + thisDayOfMonth + " " + thisMonth);
+        this.thisDayOfWeek = daysOfWeek[date.get(Calendar.DAY_OF_WEEK) - 1];
+        this.thisDayOfMonth = date.get(Calendar.DAY_OF_MONTH);
+        this.thisMonth = months[date.get(Calendar.MONTH)];
+        title = thisDayOfWeek + " " + thisDayOfMonth + " " + thisMonth;
+        MainActivity.toolbar.setTitle(title);
+
+        this.title += " " + getResources().getString(R.string.share_title);
+        this.fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        if (! MainActivity.calendar.get(date).get("event").isEmpty()) {
+            title += "\n" + getResources().getString(R.string.event) + ": " + MainActivity.calendar.get(date).get("event");
+            if (! MainActivity.calendar.get(date).get("food").isEmpty())
+                title += "\n" + getResources().getString(R.string.food) + ": " + MainActivity.calendar.get(date).get("food");
+        } else
+            title += "\n" + getResources().getString(R.string.open);
+        title += "\n\n" + getResources().getString(R.string.share_text) + "\n" + Uri.parse("https://play.google.com/store/apps/details?id=" + getContext().getPackageName()).toString();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, title);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+//                Uri imageUri = Uri.parse("file//res/drawable/logo.png");
+//                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("android.resource://"
+//                        + getContext().getPackageName() + "/"
+//                        + R.drawable.logo));
+//                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("android.resource://com.lucazanrosso.anguriara/drawable/logo"));
+//                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getResources().getResourcePackageName(R.drawable.logo) + '/' + getResources().getResourceTypeName(R.drawable.logo) + '/' + getResources().getResourceEntryName(R.drawable.logo)));
+//                sendIntent.setType("image/png");
+
+//                startActivity(Intent.createChooser(sendIntent, "image"));
+//                try {
+//                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getContext().getPackageName())));
+//                } catch (android.content.ActivityNotFoundException anfe) {
+//                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getContext().getPackageName())));
+//                }
+            }
+        });
 
         ViewPager mPager = (ViewPager) view.findViewById(R.id.pager);
         PagerAdapter mPagerAdapter = new ScreenSlidePagerAdapter(getActivity().getSupportFragmentManager());
@@ -45,13 +93,34 @@ public class DayScreenSlidePagerFragment extends Fragment {
             public void onPageSelected(int position) {
                 GregorianCalendar date = MainActivity.days.get(position);
 
-                String[] daysOfWeek = getResources().getStringArray(R.array.days_of_week);
-                String[] months = getResources().getStringArray(R.array.months);
+                daysOfWeek = getResources().getStringArray(R.array.days_of_week);
+                months = getResources().getStringArray(R.array.months);
 
-                String thisDayOfWeek = daysOfWeek[date.get(Calendar.DAY_OF_WEEK) - 1];
-                int thisDayOfMonth = date.get(Calendar.DAY_OF_MONTH);
-                String thisMonth = months[date.get(Calendar.MONTH)];
-                MainActivity.toolbar.setTitle(thisDayOfWeek + " " + thisDayOfMonth + " " + thisMonth);
+                thisDayOfWeek = daysOfWeek[date.get(Calendar.DAY_OF_WEEK) - 1];
+                thisDayOfMonth = date.get(Calendar.DAY_OF_MONTH);
+                thisMonth = months[date.get(Calendar.MONTH)];
+                title = thisDayOfWeek + " " + thisDayOfMonth + " " + thisMonth;
+                MainActivity.toolbar.setTitle(title);
+
+                title += " " + getResources().getString(R.string.share_title);
+                if (! MainActivity.calendar.get(date).get("event").isEmpty()) {
+                    title += "\n" + getResources().getString(R.string.event) + ": " + MainActivity.calendar.get(date).get("event");
+                    if (! MainActivity.calendar.get(date).get("food").isEmpty())
+                        title += "\n" + getResources().getString(R.string.food) + ": " + MainActivity.calendar.get(date).get("food");
+                } else
+                    title += "\n" + getResources().getString(R.string.open);
+                title += "\n\n" + getResources().getString(R.string.share_text) + "\n" + Uri.parse("https://play.google.com/store/apps/details?id=" + getContext().getPackageName()).toString();
+
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent sendIntent = new Intent();
+                        sendIntent.setAction(Intent.ACTION_SEND);
+                        sendIntent.putExtra(Intent.EXTRA_TEXT, title);
+                        sendIntent.setType("text/plain");
+                        startActivity(sendIntent);
+                    }
+                });
             }
 
             @Override
