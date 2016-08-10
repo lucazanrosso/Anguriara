0f7c55a8ab665b94edb1e3ce4d4dbb62fc9f68bb
Luca Zanrosso <lucazanrosso94@gmail.com>
2016-08-10 12:36:18 +0200

    Start to build notification service

--- app/src/main/java/com/lucazanrosso/anguriara/BadDayService.java
+++ app/src/main/java/com/lucazanrosso/anguriara/BadDayService.java
@@ -1,6 +1,7 @@
 package com.lucazanrosso.anguriara;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -11,6 +12,15 @@ import android.os.Process;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.File;
+import java.util.GregorianCalendar;
+
 public class BadDayService extends Service {
 
     private Looper mServiceLooper;
@@ -18,22 +28,80 @@ public class BadDayService extends Service {
 
     // Handler that receives messages from the thread
     private final class ServiceHandler extends Handler {
-        public ServiceHandler(Looper looper) {
+        Context context;
+
+        public ServiceHandler(Looper looper, Context context) {
             super(looper);
+            this.context = context;
         }
         @Override
         public void handleMessage(Message msg) {
             // Normally we would do some work here, like download a file.
             // For our sample, we just sleep for 5 seconds.
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                // Restore interrupt status.
-                Thread.currentThread().interrupt();
-            }
-            // Stop the service using the startId, so that we don't stop
-            // the service in the middle of handling another job
-            stopSelf(msg.arg1);
+//            try {
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                // Restore interrupt status.
+//                Thread.currentThread().interrupt();
+//            }
+//            // Stop the service using the startId, so that we don't stop
+//            // the service in the middle of handling another job
+//            stopSelf(msg.arg1);
+
+            final FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("test");
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Intent notificationIntent = new Intent(context, MyNotification.class);
+                    notificationIntent.putExtra("notification_text", context.getResources().getString(R.string.bad_weather));
+                    notificationIntent.putExtra("id", dataSnapshot.child("id").getValue(Integer.class));
+                    context.sendBroadcast(notificationIntent);
+
+//                if (dataSnapshot.child("bad_weather").getValue(Boolean.class)) {
+//                    NotificationCompat.Builder mBuilder =
+//                            new NotificationCompat.Builder(context)
+//                                    .setSmallIcon(R.drawable.notification)
+//                                    .setContentTitle(context.getResources().getString(R.string.this_evening))
+//                                    .setContentText(context.getResources().getString(R.string.bad_weather))
+//                                    .setDefaults(Notification.DEFAULT_SOUND);
+//                    Intent resultIntent = new Intent(context, MyNotification.class);
+//                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+//                    stackBuilder.addParentStack(MainActivity.class);
+//                    stackBuilder.addNextIntent(resultIntent);
+//                    PendingIntent resultPendingIntent =
+//                            stackBuilder.getPendingIntent(
+//                                    0,
+//                                    PendingIntent.FLAG_UPDATE_CURRENT
+//                            );
+//                    mBuilder.setContentIntent(resultPendingIntent);
+//                    MainActivity.notificationPendingIntent = PendingIntent.getBroadcast(context, -1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//                    NotificationManager mNotificationManager =
+//                            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//                    mNotificationManager.notify(0, mBuilder.build());
+//                } else {
+//                    boolean isBadDay = dataSnapshot.child("bad_weather").getValue(Boolean.class);
+//                    if (isBadDay) {
+//                        MainActivity.badDay = new GregorianCalendar(dataSnapshot.child("year").getValue(Integer.class), dataSnapshot.child("month").getValue(Integer.class), dataSnapshot.child("day").getValue(Integer.class));
+//                        serializeBadDay(context);
+//                        if (!MainActivity.today.equals(MainActivity.badDay)) {
+//                            badDay = null;
+//                            new File(getFilesDir(), "bad_day.ser").delete();
+//                        }
+//                    } else if (MainActivity.badDay != null) {
+//                        badDay = null;
+//                        new File(getFilesDir(), "bad_day.ser").delete();
+//                    }
+//                    CalendarFragment.thisDayText.setText(CalendarFragment.setDateText(MainActivity.today, context));
+//                    CalendarFragment.thisDayImage.setImageResource(CalendarFragment.setThisDayImage(MainActivity.today));
+                }
+//            }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
     }
 
@@ -49,7 +117,7 @@ public class BadDayService extends Service {
 
         // Get the HandlerThread's Looper and use it for our Handler
         mServiceLooper = thread.getLooper();
-        mServiceHandler = new ServiceHandler(mServiceLooper);
+        mServiceHandler = new ServiceHandler(mServiceLooper, this);
     }
 
     @Override
--- app/src/main/java/com/lucazanrosso/anguriara/MyNotification.java
+++ app/src/main/java/com/lucazanrosso/anguriara/MyNotification.java
@@ -1,39 +1,44 @@
 package com.lucazanrosso.anguriara;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 
 public class MyNotification extends BroadcastReceiver{
 
+    int notificationId;
     String notificationTitle;
     String notificationText;
     int notificationIcon;
+    final static String GROUP_NOTIFICATION = "group_notification";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-            this.notificationTitle = context.getResources().getString(R.string.this_evening);
-            this.notificationText = intent.getStringExtra("notification_text");
-            this.notificationIcon = R.drawable.notification;
+        this.notificationId = intent.getIntExtra("id", 0);
+        this.notificationTitle = context.getResources().getString(R.string.this_evening);
+        this.notificationText = intent.getStringExtra("notification_text");
+        this.notificationIcon = R.drawable.notification;
 
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
                 .setSmallIcon(this.notificationIcon)
                 .setContentTitle(this.notificationTitle)
                 .setContentText(this.notificationText)
-                .setColor(ContextCompat.getColor(context, R.color.accent));
+                .setGroup(GROUP_NOTIFICATION)
+                .setGroupSummary(true)
+                .setColor(ContextCompat.getColor(context, R.color.accent))
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setAutoCancel(true);
         Intent resultIntent = new Intent(context, MainActivity.class);
         //resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(resultPendingIntent);
-        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
-        mBuilder.setAutoCancel(true);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(1, mBuilder.build());
+        NotificationManagerCompat mNotificationManagerCompat =
+                NotificationManagerCompat.from(context);
+        mNotificationManagerCompat.notify(notificationId, mBuilder.build());
     }
 }
