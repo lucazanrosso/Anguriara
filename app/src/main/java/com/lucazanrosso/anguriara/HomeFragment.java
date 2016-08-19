332b501c0738cc42e3b782255fc8dd45c5b5b1b7
Luca Zanrosso <lucazanrosso94@gmail.com>
2016-08-19 15:27:57 +0200

    Continue to build the new HomeFragment

--- app/src/main/java/com/lucazanrosso/anguriara/HomeFragment.java
+++ app/src/main/java/com/lucazanrosso/anguriara/HomeFragment.java
@@ -5,16 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         MainActivity.toolbar.setTitle(getResources().getString(R.string.anguriara));
 
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        TextView titleTextView = (TextView) view.findViewById(R.id.this_day_title);
+        titleTextView.setText(CalendarFragment.setDateTitle(MainActivity.today));
 
-
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
