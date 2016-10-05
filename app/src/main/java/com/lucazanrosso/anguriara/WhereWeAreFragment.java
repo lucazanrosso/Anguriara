4c79ba8327b9ea273846995bd1923226d78f4181
Luca Zanrosso <lucazanrosso94@gmail.com>
2016-10-05 15:15:19 +0200

    Save CameraPosition state when screen rotates in WhereWeAreFragment

--- app/src/main/java/com/lucazanrosso/anguriara/WhereWeAreFragment.java
+++ app/src/main/java/com/lucazanrosso/anguriara/WhereWeAreFragment.java
@@ -1,6 +1,7 @@
 package com.lucazanrosso.anguriara;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +19,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class WhereWeAreFragment extends Fragment {
 
     private GoogleMap mGoogleMap;
+    LatLng latLng;
+    float zoom;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -27,18 +36,32 @@ public class WhereWeAreFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_where_we_are, container, false);
 
+        if (savedInstanceState == null) {
+            latLng = new LatLng(45.7053472, 11.393271);
+            zoom = 12.5f;
+        } else {
+            latLng = new LatLng(savedInstanceState.getDouble("lat"), savedInstanceState.getDouble("lng"));
+            zoom = savedInstanceState.getFloat("zoom");
+        }
         SupportMapFragment mMapFragment = SupportMapFragment.newInstance();
         mMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 mGoogleMap = googleMap;
-                LatLng anguriaraLatLng = new LatLng(45.7053472, 11.393271);
-                mGoogleMap.addMarker(new MarkerOptions().position(anguriaraLatLng).title("Anguriara"));
-                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(anguriaraLatLng, 15));
+                mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(45.7053472, 11.393271)).title("Anguriara"));
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
             }
         });
         getChildFragmentManager().beginTransaction().replace(R.id.my_location_map_anchor, mMapFragment).addToBackStack("secondary").commit();
 
         return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putDouble("lat", mGoogleMap.getCameraPosition().target.latitude);
+        outState.putDouble("lng", mGoogleMap.getCameraPosition().target.longitude);
+        outState.putFloat("zoom", mGoogleMap.getCameraPosition().zoom);
     }
 }
