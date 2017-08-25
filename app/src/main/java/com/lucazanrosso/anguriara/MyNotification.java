364e2a3d96086f2131c22d665110188c2df060ce
lucazanrosso <lucazanrosso94@gmail.com>
2017-08-25 14:13:03 +0200

    Now notifications work in android Oreo

--- app/src/main/java/com/lucazanrosso/anguriara/MyNotification.java
+++ app/src/main/java/com/lucazanrosso/anguriara/MyNotification.java
@@ -1,13 +1,17 @@
 package com.lucazanrosso.anguriara;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class MyNotification extends BroadcastReceiver{
 
@@ -21,7 +25,7 @@ public class MyNotification extends BroadcastReceiver{
         this.notificationText = intent.getStringExtra("notification_text");
         this.notificationIcon = R.drawable.notification;
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "1")
                 .setSmallIcon(this.notificationIcon)
                 .setContentTitle(this.notificationTitle)
                 .setContentText(this.notificationText)
@@ -33,8 +37,15 @@ public class MyNotification extends BroadcastReceiver{
         Intent resultIntent = new Intent(context, MainActivity.class);
         PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManagerCompat mNotificationManagerCompat =
-                NotificationManagerCompat.from(context);
-        mNotificationManagerCompat.notify(0, mBuilder.build());
+
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("1",
+                    "Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
+        mNotificationManager.notify(0, mBuilder.build());
     }
 }
--- app/src/main/java/com/lucazanrosso/anguriara/NotificationService.java
+++ app/src/main/java/com/lucazanrosso/anguriara/NotificationService.java
@@ -49,7 +49,7 @@ public class NotificationService extends Service {
 //            stopSelf(msg.arg1);
 
             sharedPreferences = getSharedPreferences("PREFERENCES", Context.MODE_PRIVATE);
-            DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("notification");
+            DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("test");
             myRef.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
